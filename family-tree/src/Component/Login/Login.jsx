a13dd630fcd8b7061464a48b3fbfeeb21d600e18
Raza Muhammad <waistband161@gmail.com>
2024-03-20 00:18:11 +0500

    2 pages done

--- family-tree/src/Component/Login/Login.jsx
+++ family-tree/src/Component/Login/Login.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Login.scss'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
   return (
@@ -9,33 +10,39 @@ const Login = () => {
            <div>
 
             <h1 className='welcome'>Welcome to Family Tree</h1>
-            <h1 className='app'>Applicaion</h1>
+            <h1 className='app'>Application</h1>
            </div>
         </div>
         <div className="right">
+
+          <div className="box">
+            
+            <div className="hed">
             <h3>Log in to your account</h3>
             <p>Welcome back! Please enter your details.</p>
+            </div>
 
-            <div>
+            <div className='input'>
             <label htmlFor="email">Email</label><br />
-            <input type="email" name="email" id="email" />
+            <input type="email" name="email" id="email" placeholder='Enter your email'/>
             </div>
 
             
-            <div>
+            <div className='input'>
             <label htmlFor="password">Password</label><br />
-            <input type="password" name="password" id="password" />
+            <input type="password" name="password" id="password" placeholder='Enter your password'/>
             </div>
 
-            <div>
-              <input type="checkbox" name="remember me" id="remember" />
-              <label htmlFor="remember me">Remember Password</label>
-
+            <div id='remember'>
+              <div>
+              <input type="checkbox" name="remember me" />
+              <label htmlFor="remember me">Remember Password</label> </div>
               <a href="#">Forgot Password?</a>
             </div>
 
-            <button>Log in</button>
-            <button>Sign up</button>
+            <button className='login'>Log in</button>
+          <Link to="/signup"><button className='signup'>Sign up</button></Link>  
+          </div>
 
         </div>
       </div>
--- family-tree/src/Component/Signup/Signup.jsx
+++ family-tree/src/Component/Signup/Signup.jsx
@@ -1,11 +1,57 @@
 import React from 'react'
+import './Signup.scss'
+import { Link } from 'react-router-dom'
+
 
 const Signup = () => {
-  return (
+  
+      return (
     <div>
-      SignUp page
+      <div className="main">
+        <div className="left">
+           <div>
+
+            <h1 className='welcome'>Welcome to Family Tree</h1>
+            <h1 className='app'>Application</h1>
+           </div>
+        </div>
+        <div className="right">
+
+          <div className="box">
+            
+            <div className="hed">
+            <h3>Sign up to your account</h3>
+            <p>Welcome back! Please enter your details.</p>
+            </div>
+
+            <div className='input'>
+            <label htmlFor="name">Name</label><br />
+            <input type="email" name="name" id="name" placeholder='Enter your name'/>
+            </div>
+
+
+            <div className='input'>
+            <label htmlFor="email">Email</label><br />
+            <input type="email" name="email" id="email" placeholder='Enter your email'/>
+            </div>
+
+            
+            <div className='input'>
+            <label htmlFor="password">Password</label><br />
+            <input type="password" name="password" id="password" placeholder='Enter your password'/>
+            </div>
+
+             
+            <Link to="/"> <button className='login'>Create Account</button> </Link>
+
+          </div>
+
+        </div>
+      </div>
     </div>
   )
+    
+  
 }
 
 export default Signup
