0be0ea36728182665ba6bd5d97af12427e98a190
Raza Muhammad <waistband161@gmail.com>
2024-04-21 00:15:01 +0500

    setup done

---
+++ Social Media Web/src/App.tsx
@@ -0,0 +1,13 @@
+import './global.css'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">
+      Hello Snapbook!
+    </h1>
+    </div>
+  )
+}
+
+export default App
